f148fe35e3e290db68ddeb97fb8287b791d5ce7c
maxalsol <max.pallat@gmail.com>
2023-02-25 12:58:38 +1300

    variable comparison continued

--- JavaScript/01_basic/01_basic-variables/variableComparison.js
+++ JavaScript/01_basic/01_basic-variables/variableComparison.js
@@ -25,4 +25,21 @@ console.log(a <= b); // true
 // check if one value is greater than or equal to the other
 console.log(a >= b); // false
 
+// STRINGS
+let d = 'hi';
+let e = 'hello';
+let f = 'hi';
+
+// check if equal to one another ==
+console.log(d == e); // false
+console.log(d == f); // true
+
+// check if not equal to one another !=
+console.log(d != e); // true
+console.log(d != f); // false
+
+// greater or less than comparisons possible because JS sorted alphabetically
+console.log(d < e);
+console.log(d > e);
+
 //* Exercise
