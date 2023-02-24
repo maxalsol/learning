179e18fbff7098149b6ce412e70c71fc28298ca1
maxalsol <max.pallat@gmail.com>
2023-02-24 18:55:46 +1300

    variableComparison started

---
+++ JavaScript/01_basic/01_basic-variables/variableComparison.js
@@ -0,0 +1,28 @@
+//? VARIABLE COMPARISON
+
+// NUMBERS
+let a = 1;
+let b = 2;
+let c = 1;
+
+// check if equal to one another ==
+console.log(a == b); // false
+console.log(a == c); // true
+
+// check if not equal to one another !=
+console.log(a != b); // true
+console.log(a != c); // false
+
+// check if one value is less than the other
+console.log(a < b); // true
+
+// check if one value is greater than the other
+console.log(a > b); // false
+
+// check if one value is less than or equal to the other
+console.log(a <= b); // true
+
+// check if one value is greater than or equal to the other
+console.log(a >= b); // false
+
+//* Exercise
