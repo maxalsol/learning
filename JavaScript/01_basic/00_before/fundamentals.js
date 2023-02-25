269f2a0dcc6d7ad472b11a6e760cd225436fb51e
maxalsol <max.pallat@gmail.com>
2023-02-25 17:33:30 +1300

    files renamed

--- JavaScript/01_basic/00_before/fundamentals.js
+++ JavaScript/01_basic/00_before/fundamentals.js
@@ -1,3 +1,24 @@
 // JavaScript is
 // .. synchronous
 // .. single-threaded
+
+//? COMMENTS
+// single line comment for short instructions
+
+/* multi
+line
+comment
+for large instructions and info */
+
+// comments don´t run, just human readable text that explain the code
+// shall help understand and refence code for one self and others
+
+/* COMMAND + / 
+comments out selected area as a single line comments */
+
+// ? GARBAGE COLLECTION
+// JS it determines which variables are used and which are not
+// takes variables that are no longer used and removes them from memory
+// if variable is needed JS keeps it until it´s no longer needed
+// happens while programm is running
+// makes sure the memory is as clean as possible
