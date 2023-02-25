231624b4dabd558751964f87bfcbef6b69106483
maxalsol <max.pallat@gmail.com>
2023-02-25 19:40:08 +1300

    normalFunctions started

---
+++ JavaScript/01_basic/02_functions-and-scoping/01_normalFunctions.js
@@ -0,0 +1,22 @@
+// ? NORMAL FUNCTIONS
+// used to create repeatable logic
+// named functions have a name
+// anonymous functions don´t
+// information can bepassed to functions as arguments inside ()
+// code goes in between curly braces
+function sayHi() {
+    console.log('Hi');
+}
+
+// functions have to be called to run them
+// name of function followed by () will do so
+// if function takes no arguments () be left blank
+sayHi();
+
+//* EXERCISE
+//create a function that takes no arguments and prints out a name
+function printName() {
+    console.log('Zoe');
+}
+
+printName();
