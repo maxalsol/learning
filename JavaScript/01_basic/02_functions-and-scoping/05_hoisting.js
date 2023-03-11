4c0e410d3bc408519f2f9d5a14d75936558b1cde
maxalsol <max.pallat@gmail.com>
2023-03-11 13:46:23 +1300

    05_hoisting started

---
+++ JavaScript/01_basic/02_functions-and-scoping/05_hoisting.js
@@ -0,0 +1,12 @@
+//? HOISTING
+// everything in JS goes from top to bottom
+// we can´t declare a variable after the funtion call
+// but you can with functions
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(1, 2));
+
+//* EXERCISES
