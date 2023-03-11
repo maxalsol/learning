daa4088528b62cd9afb765fe7a24fd35a0b40dfc
maxalsol <max.pallat@gmail.com>
2023-03-11 20:27:17 +1300

    05_hoisting continued

--- JavaScript/01_basic/02_functions-and-scoping/05_hoisting.js
+++ JavaScript/01_basic/02_functions-and-scoping/05_hoisting.js
@@ -3,10 +3,14 @@
 // we can´t declare a variable after the funtion call
 // but you can with functions
 
+console.log(sum(1, 2));
+
 function sum(a, b) {
     return a + b;
 }
 
-console.log(sum(1, 2));
+// before running code JS goes through the code and pretends to move functions to the top of file
+// that´s what hoisting does
+// does not work with arrow functions because they are defined as a variable
 
 //* EXERCISES
