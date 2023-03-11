8aeb5ab6d2ec780b4995b2fa054cd9e632a93e8c
maxalsol <max.pallat@gmail.com>
2023-03-11 13:39:16 +1300

    notes added

--- JavaScript/01_basic/02_functions-and-scoping/04_stackTraceAndCallStack.js
+++ JavaScript/01_basic/02_functions-and-scoping/04_stackTraceAndCallStack.js
@@ -1,4 +1,10 @@
 //? STACK TRACE AND CALL STACK
 // stack trace tells you in what order functions were called
+// first thing in the call stack is the most recent function that was called
+// works backwards from inside to outside
+// first definition is the most inside function where error occured
+// last thing is the outer most function
+
+// good for error tracking and tracing
 
 //*
