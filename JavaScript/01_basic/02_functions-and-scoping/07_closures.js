862172dedd8feb84854ca1570d74e7becbdf594a
maxalsol <max.pallat@gmail.com>
2023-03-13 18:27:15 +1300

    closures continued

--- JavaScript/01_basic/02_functions-and-scoping/07_closures.js
+++ JavaScript/01_basic/02_functions-and-scoping/07_closures.js
@@ -9,5 +9,14 @@ function print() {
 a = 2;
 
 print();
+// print() prints out 2 because it shows the most recent value of what a is
+
+// usually closures are defined as functions insde functions
+function printOut(variable) {
+    function func(variable2) {
+        console.log(variable);
+        console.log(variable2);
+    }
+}
 
 //* EXERCISES
