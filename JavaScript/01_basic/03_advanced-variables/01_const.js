8cdbcbf7d10bbd9cd45455ac3e796cae44bcd4eb
maxalsol <max.pallat@gmail.com>
2023-03-15 08:05:59 +1300

    const continued

--- JavaScript/01_basic/03_advanced-variables/01_const.js
+++ JavaScript/01_basic/03_advanced-variables/01_const.js
@@ -1,4 +1,15 @@
 //? CONST
+// define variables with the keyword const instead of let
 // can´t be left empty.. needs at least NULL or UNDEFINED
+// const stands for constant or unchangeing
+// if we try to change the value of a we get an error
+
+const a = 1;
+
+a = 2;
+
+console.log(a);
+
+// if we don´t want to change the value of a variable we use const instead of let
 
 //* EXERCISES
