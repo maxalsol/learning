39131701dc254386c7e7988c1c6201b8767747fa
maxalsol <max.pallat@gmail.com>
2023-03-15 13:07:41 +1300

    02_typeCoercion started

---
+++ JavaScript/01_basic/03_advanced-variables/02_typeCoercion.js
@@ -0,0 +1,10 @@
+//? TYPE COERCION
+// way of converting one type of info to another type
+// main types we woryy about are:
+//    numbers
+//    strings
+//    boolean
+//    null
+//    undefined
+
+//* EXERCISE
