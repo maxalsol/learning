e695153987803cfb06bb173a5ea10f05f819463e
maxalsol <max.pallat@gmail.com>
2023-03-15 13:23:24 +1300

    02_typeCoercion continued

--- JavaScript/01_basic/03_advanced-variables/02_typeCoercion.js
+++ JavaScript/01_basic/03_advanced-variables/02_typeCoercion.js
@@ -1,10 +1,25 @@
 //? TYPE COERCION
 // way of converting one type of info to another type
-// main types we woryy about are:
+// main types we worry about are:
 //    numbers
 //    strings
 //    boolean
 //    null
 //    undefined
+
+// a few handy functions already inside JS
+let a = '1';
+console.log(a);
+console.log(typeof a);
+
+// string to full number (integer)
+let b = '2';
+console.log(parseInt(b));
+console.log(typeof parseInt(b));
+
+// string to decimal number (float)
+let c = '3';
+console.log(parseFloat(c));
+console.log(typeof parseFloat(c));
 
 //* EXERCISE
