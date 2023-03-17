e161e58eb64079e5d174afd283880e4e52fad58a
maxalsol <max.pallat@gmail.com>
2023-03-17 16:42:13 +1300

    arrays started

---
+++ JavaScript/01_basic/03_advanced-variables/05_arrays.js
@@ -0,0 +1,9 @@
+//? ARRAYS
+// list of values
+// arrays are defined by []
+// items get delimited by ,
+// arrays start counting at index 0
+
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(a);
+// index 0 holds value of 1, index 1 holds value of 2, ...
