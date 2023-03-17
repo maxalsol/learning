a350e350cf76db993d3d6374512a729f7249cf30
maxalsol <max.pallat@gmail.com>
2023-03-17 21:13:34 +1300

    06_objects updated

--- JavaScript/01_basic/03_advanced-variables/06_objects.js
+++ JavaScript/01_basic/03_advanced-variables/06_objects.js
@@ -1,28 +1,31 @@
 //? OBJECTS
-// a collection of information all related together in some way
+// collection of information all related together in some way
 // instead of 100 single variables for one thing (person i.e.) we create an object
 // to create an object we use {}
 
-let animal = {}; // empty object
+let emptyObject = {}; // empty object
 
 // objects contain key:value pairs
 // keys like name, age, favorite beer, ..
 // pairs then hold the value liek Max, 29, Furphy, ..
 // key:value pairs get seperated by ,
+// key names need always to be different (like variables)
 
-let person = {
-    name: 'Max',
-    age: 29,
-    favoriteBeer: 'Furphy'
+let pet = {
+    animalType: 'cat',
+    furColor: 'red',
+    age: 7,
+    favoriteFood: 'Lasagna',
+    isObese: true
 };
 
-console.log(person);
+console.log(pet);
 
 // to access one part of the object we can use dot-notation
 // dot says we want a single property (key:value pair) from the object
 // we get value from the desired property
-console.log(person.name);
-console.log(person.age);
+console.log(pet.age);
+console.log(pet.isObese);
 
 // console is actually an object
 // if we use log we use a function (method) of the console object
@@ -33,7 +36,7 @@ console.log(console.log);
 // can be an anonymous function because it is declared by the key´s name
 let myCar = {
     brand: 'VW',
-    make: 'Bora',
+    model: 'Bora',
     color: 'black',
     cylinders: 5,
     ccm: 2324,
@@ -43,6 +46,7 @@ let myCar = {
     revEngine: function () {
         console.log('vroom');
     },
+    // MOST USED
     honkHorn() {
         console.log('beep, beep');
     }
@@ -52,4 +56,51 @@ let myCar = {
 myCar.revEngine();
 myCar.honkHorn();
 
+// objects can be put inside other objects
+// even arrays can be put inside objects
+let person = {
+    name: 'Max',
+    hobbies: ['coding', 'traveling', 'cooking', 'calisthenics'],
+    address: {
+        street: '420 Green Street',
+        city: 'Berlin',
+        postCode: 10001
+    }
+};
+
+console.log(person.name);
+console.log(person.hobbies);
+console.log(person.address);
+
+// we can access elements on the array
+console.log(person.hobbies[2]); // cooking
+
+// we can chain object properties together to get the desired information
+console.log(person.address.city); // Berlin
+
 //* EXERCISE
+// create an object called book with the following properties
+// 1. title - string
+// 2. author - object with properties name and age
+
+let book = {
+    title: 'The Four Agreements',
+    author: {
+        name: 'Miguel Ruiz',
+        age: 70
+    }
+};
+
+console.log(book);
+console.log(book.title);
+console.log(book.author.name);
+console.log(book.author.age);
+
+// when we try to access a property of an object that doesn´t exist we get undefined
+console.log(book.pages);
+
+// object properties can be changed
+book.title = 'The Fifth Agreement';
+book.author.age = 71;
+
+console.log(book);
