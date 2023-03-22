cbd962af101ecef02f4ec23ad6a44a64ed3a220b
maxalsol <max.pallat@gmail.com>
2023-03-23 12:50:18 +1300

    parsing HTML and JS continued

--- JavaScript/01_basic/04_basic-DOM/01_asyncVsDefer.js
+++ JavaScript/01_basic/04_basic-DOM/01_asyncVsDefer.js
@@ -1,10 +1,15 @@
 //? ASYNC VS DEFER
 // two different ways of parsing HTML
 
-//! ASYNC
-// asynchronously downloads JS file in the background
-
 //! DEFER
 // defers execution of the file until entire document is parsed
+// same function as putting script tag at bottom of body
+// scriptnjust loads before page loads essentially
+// if all script tags have defer attribute they run in order top to bottom
 
-//* EXERCISE
+//! NORMAL
+// to have best result put script at bottom of body
+
+//! ASYNC
+// asynchronously downloads JS file in the background
+// executes as soon as it finishes downloading
