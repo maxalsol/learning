abb8673f8c95bebd98c621bf928a93bdf2e45504
maxalsol <max.pallat@gmail.com>
2023-03-23 07:55:48 +1300

    parsing HTML and JS

---
+++ JavaScript/01_basic/04_basic-DOM/01_asyncVsDefer.js
@@ -0,0 +1,10 @@
+//? ASYNC VS DEFER
+// two different ways of parsing HTML
+
+//! ASYNC
+// asynchronously downloads JS file in the background
+
+//! DEFER
+// defers execution of the file until entire document is parsed
+
+//* EXERCISE
