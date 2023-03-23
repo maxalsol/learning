a5e3d13ac07e29c3aa636152a9c07710df72c71d
maxalsol <max.pallat@gmail.com>
2023-03-23 13:25:54 +1300

    window object

---
+++ JavaScript/01_basic/04_basic-DOM/02_windowObject.js
@@ -0,0 +1,12 @@
+//? WINDOW OBJECT
+// global object for entire browser
+// loads of different properties
+// if we code something JS assumes that we always use the window object..
+//   -> like with console.log
+//   -> actually window.console.log
+console.log('console.log');
+window.console.log('window.console.log');
+
+console.log(window);
+
+//* EXERCISE
